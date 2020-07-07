8697a8769222cf9cb298822bdf956ebdbb9b476a
shandryll <shandryll94@gmail.com>
2020-07-07 13:29:46 -0300

    wip

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -3,14 +3,14 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
-import Routes from '../main/routes';
 import Menu from '../template/menu';
+import Routes from '../main/routes';
 
 export default (props) => {
   return (
     <div className="container">
-      <Routes></Routes>
       <Menu></Menu>
+      <Routes></Routes>
     </div>
   )
 };
--- todo-app/frontend/src/main/routes.jsx
+++ todo-app/frontend/src/main/routes.jsx
@@ -4,12 +4,12 @@ import { Router, Route, Redirect, hashHistory } from 'react-router';
 import Todo from '../todo/todo';
 import About from '../about/about';
 
-export default props=> {
+export default (props) => {
   return (
     <Router history={hashHistory}>
-      <Redirect from="*" to="/todos"></Redirect>
-      <Route path="/todos" component={Todo}></Route>
-      <Route path="/about" component={About}></Route>
+      <Route path='/todos' component={Todo} />
+      <Route path='/about' component={About} />
+      <Redirect from='*' to='/todos' />
     </Router>
   )
 }
