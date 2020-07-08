aa4386dac3789411c5bc6dbd1d4e650d3da5dc74
shandryll <shandryll94@gmail.com>
2020-07-08 13:32:36 -0300

    Correcao de visualziacao

--- todo-app/frontend/src/main/routes.jsx
+++ todo-app/frontend/src/main/routes.jsx
@@ -6,8 +6,8 @@ import About from '../about/about'
 
 export default props => (
   <Router history={hashHistory}>
-    <Route path='/todos' component={Todo} />
-    <Route path='/about' component={About} />
-    <Redirect from='*' to='/todos' />
+      <Route path='/todos' component={Todo} />
+      <Route path='/about' component={About} />
+      <Redirect from='*' to='/todos' />
   </Router>
 )
